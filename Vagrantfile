12c6e58317d3b771e644ca8e5b83770f121d20df
kosteek <kosteek911@gmail.com>
2021-06-22 02:17:01 +0300

    init

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ Vagrant.configure("2") do |config|
 
   config.vm.box = "hashicorp/bionic64"
   config.vm.network "forwarded_port", guest: 8088, host: 8088
-  config.vm.synced_folder "app/", "/home/vagrant/app"
+  config.vm.synced_folder ".", "/home/vagrant/app"
   config.vm.provider "virtualbox" do |vb|
     vb.name = "Docker_lesson3"
     vb.memory = "1024"
---
+++ scripts/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release git
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker vagrant
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose up -f docker-compose_go.yaml
